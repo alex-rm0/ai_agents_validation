7525ba0584e567e7fcf5d38cecc2611442479033
Alexandre Magalhães <alexandre.magalhaes@devlop.systems>
2026-03-30 14:08:04 +0100

    sync

--- test_engine/obj/Debug/net8.0/FunctionalTests.AssemblyInfo.cs
+++ test_engine/obj/Debug/net8.0/FunctionalTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunctionalTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7a64afcfbc81c6b9159d30b374fec0a66e7b2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70bed10dcbe1608751c634e75c25d104e37a6cb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunctionalTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunctionalTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
